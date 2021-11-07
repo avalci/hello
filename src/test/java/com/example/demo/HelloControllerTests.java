a970f3c325ff0465fd75de4a63876a541f5f5240
Avalci Suzano <avalci@gmail.com>
2021-11-07 14:59:24 -0300

    hello controller and hello controller test

---
+++ src/test/java/com/example/demo/HelloControllerTests.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HelloControllerTests {
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@LocalServerPort
+	private int port;
+
+	@Test
+	void test() {
+		ResponseEntity<String> entity = restTemplate.exchange("http://localhost:" + port, HttpMethod.GET, null,
+				String.class);
+
+		assertAll(
+				() -> assertEquals(HttpStatus.OK, entity.getStatusCode()), 
+				() -> assertEquals("hello", entity.getBody())
+		);
+	}
+
+}
