d06e4acd524fa316906d25e394964e380e601e10
Avalci Suzano <avalci@gmail.com>
2021-11-11 23:26:18 -0300

    adding comment 2

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * HelloController
+ * HelloController ()
  */
 @RestController
 public class HelloController {
